13146b524e3d9091b9d561cf292e9c25a5f5f87f
hamitmizrak <hamitmizrak@gmail.com>
2022-08-07 15:43:18 +0300

    js

---
+++ frontend/javascript/template.js
@@ -0,0 +1,16 @@
+//single comment : comment out
+/* multiple comment */
+//alert("Merhabalar")
+
+console.log("Merhabalar");
+console.info("info");
+console.warn("warning")
+console.error("error")
+
+//alert("pop-up")
+//window.alert("pop-up-2")
+
+document.write("write verisi")
+document.writeln("<br/>Benden sonra gelen alt satırda başlar")
+
+
