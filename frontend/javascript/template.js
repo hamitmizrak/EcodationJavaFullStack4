d659f76b58e1405363e723380dd51a88380bca5e
hamitmizrak <hamitmizrak@gmail.com>
2022-08-07 15:55:57 +0300

    değişkenler

--- frontend/javascript/template.js
+++ frontend/javascript/template.js
@@ -2,15 +2,42 @@
 /* multiple comment */
 //alert("Merhabalar")
 
-console.log("Merhabalar");
-console.info("info");
-console.warn("warning")
-console.error("error")
+// console.log("Merhabalar");
+// console.info("info");
+// console.warn("warning")
+// console.error("error")
 
 //alert("pop-up")
 //window.alert("pop-up-2")
 
-document.write("write verisi")
-document.writeln("<br/>Benden sonra gelen alt satırda başlar")
+// document.write("write verisi")
+// document.writeln("<br/>Benden sonra gelen alt satırda başlar")
+
+//Not:
+//""  ==> Çift tırnak kullanabiliriz.
+//''  ==> Tek tırnak kullanabiliriz.
+// `` ==> backtick 
+///////////////////////////////////////
+//variable
+// var dinamik bir değerdir.
+// Variable Dikkat edeceğimiz hususlar:
+// camelCase kullanalım.
+
+// camelCase kullanalım
+var degiskenAdi = true;
+console.log(degiskenAdi);
+
+// $ ve _ ile başlayabiliriz
+var $degiskenAdi = 44;
+console.log($degiskenAdi);
+
+var _degiskenAdi = 23.55;
+console.log(_degiskenAdi);
+
+// sayılarla ile bitebilir.
+var degiskenAdi55 = "Merbalar";
+console.log(degiskenAdi55);
+
+// sayı ile ve ($ ve _) haricinden özel simgeyle başlanmaz kullanalım
 
 
