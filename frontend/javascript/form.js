15ece970bfff5703bb1cb2fe2c3d1702bda0cecc
hamitmizrak <hamitmizrak@gmail.com>
2022-08-24 18:16:32 +0300

    Form

---
+++ frontend/javascript/form.js
@@ -0,0 +1,11 @@
+
+    function formData() {
+    let first_name = document.getElementById("first_name").value;
+    let last_name = document.getElementById("last_name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    console.log(`Adı:${first_name} ${last_name} ${email}  ${password}`);
+
+    let first_name2=$('#first_name').val();
+    console.log(first_name2);
+}
