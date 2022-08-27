0ec631768a4f04ddcd7da255edb42883b0c54eb1
hamitmizrak <hamitmizrak@gmail.com>
2022-08-27 18:42:32 +0300

    Dark Light revize

--- frontend/template.js
+++ frontend/template.js
@@ -1,18 +1,23 @@
 //()();
-let nowDate=()=>{
-    let year=new Date().getFullYear();
+let nowDate = () => {
+    let year = new Date().getFullYear();
 }
 
 // backTo Top
-$(window).scroll(function(){
-    if($(this).scrollTop()>90){
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 90) {
         $('#back_top').fadeIn('slow');
-    }else{
+    } else {
         $('#back_top').fadeOut('slow');
     }
 });
 
-$("#back_top").click(function(){
-    $('html,body').animate({scrollTop: 0},90,'easeInOutExpo');
+$("#back_top").click(function () {
+    $('html,body').animate({ scrollTop: 0 }, 90, 'easeInOutExpo');
     return false;
 })
+
+// Dark Light Button
+let darkLight = () => {
+    let data = document.body.classList.toggle("dark-mode");
+}
