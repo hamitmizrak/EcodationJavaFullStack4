18d87cf83146faa55e7eaeea94738cebd05bf42a
hamitmizrak <hamitmizrak@gmail.com>
2022-08-27 17:58:40 +0300

    back top

---
+++ frontend/template.js
@@ -0,0 +1,18 @@
+//()();
+let nowDate=()=>{
+    let year=new Date().getFullYear();
+}
+
+// backTo Top
+$(window).scroll(function(){
+    if($(this).scrollTop()>90){
+        $('#back_top').fadeIn('slow');
+    }else{
+        $('#back_top').fadeOut('slow');
+    }
+});
+
+$("#back_top").click(function(){
+    $('html,body').animate({scrollTop: 0},90,'easeInOutExpo');
+    return false;
+})
