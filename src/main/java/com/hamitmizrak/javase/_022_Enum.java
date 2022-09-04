ce8f27512c92a7e9a77965ab179810f40b2d4c0d
hamitmizrak <hamitmizrak@gmail.com>
2022-09-04 16:36:22 +0300

    Enum

---
+++ src/main/java/com/hamitmizrak/javase/_022_Enum.java
@@ -0,0 +1,5 @@
+package com.hamitmizrak.javase;
+
+public enum _022_Enum {
+    PAZARTESI,SALI,CARSAMBA,PERSEMBE,CUMA,CUMARTESI,PAZAR
+}
---
+++ src/main/java/com/hamitmizrak/javase/_023_WeekDayEnum.java
@@ -0,0 +1,63 @@
+package com.hamitmizrak.javase;
+
+import java.util.Scanner;
+
+public class _023_WeekDayEnum {
+
+    //class varaible
+    private static Scanner klavye = new Scanner(System.in);
+
+    // Kullanıcı tarafından girilen hafta günü gösteren algoritma
+    private static String getUserData() {
+        System.out.println("\nLütfen  haftanın gününü giriniz ");
+        String day = klavye.nextLine();
+        return day;
+    }
+
+    private static void whoWeekDay() {
+
+        String day = getUserData().toUpperCase();
+        switch (day) {
+            //_022_Enum.PAZARTESI.toString():
+           // String day1="PAZARTESI";
+            case "PAZARTESI":
+                System.out.println("Günlerden PAZARTESI");
+                break;
+            case "SALI":
+                System.out.println("Günlerden SALI");
+                break;
+            case "CARSAMBA":
+                System.out.println("Günlerden CARSAMBA");
+                break;
+            case "PERSEMBE":
+                System.out.println("Günlerden PERSEMBE");
+                break;
+            case "CUMA":
+                System.out.println("Günlerden CUMA");
+                break;
+            case "CUMARTESI":
+                System.out.println("Günlerden CUMARTESI");
+                break;
+            case "PAZAR":
+                System.out.println("Günlerden PAZAR");
+                break;
+
+            default:
+                System.out.println("Haftanın gününü giriniz");
+                break;
+        }
+
+
+    }
+
+    //PSVM
+    public static void main(String[] args) {
+     /*   for(;;) {
+            whoWeekDay();
+        }*/
+        System.out.println(_022_Enum.CUMARTESI.toString());
+        System.out.println(_022_Enum.CUMARTESI.ordinal());
+        System.out.println(_022_Enum.CUMARTESI.values()[0]);
+
+    }
+}
