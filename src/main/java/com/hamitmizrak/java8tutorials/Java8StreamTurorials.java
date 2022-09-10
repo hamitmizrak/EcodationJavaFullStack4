9dcfa10efff0318eecd4e29c3bab2aec83857f44
hamitmizrak <hamitmizrak@gmail.com>
2022-09-10 18:51:18 +0300

    Stream method

--- src/main/java/com/hamitmizrak/java8tutorials/Java8StreamTurorials.java
+++ src/main/java/com/hamitmizrak/java8tutorials/Java8StreamTurorials.java
@@ -9,6 +9,7 @@ package com.hamitmizrak.java8tutorials;
 // 1-) Optional
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -64,14 +65,111 @@ public class Java8StreamTurorials {
         });
     }
 
+    //Sorted: stream verileri Sıralamak için kullanıyoruz
+    public static void streamSortedSmallToLargeMethod(){
+        List<String> listem=collectionListData().stream().sorted().collect(Collectors.toList());
+        listem.forEach((temp) -> {
+            System.out.println(temp);
+        });
+    }
+
+    //Sorted: stream verileri Sıralamak için kullanıyoruz
+    public static void streamSortedLargeToSmallMethod(){
+        List<String> listem=collectionListData().stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        listem.forEach((temp) -> {
+            System.out.println(temp);
+        });
+    }
+
+    //Limit: stream verileri kısıtlamak
+    public static void streamLimitMethod(){
+        List<String> listem=collectionListData().stream().sorted(Comparator.reverseOrder()).limit(5).collect(Collectors.toList());
+        listem.forEach((temp) -> {
+            System.out.println(temp);
+        });
+    }
+
+    //distinct: stream verilerinde tekrarsız veriler elde etmek
+    public static void streamDistinctMethod(){
+        List<String> listem=collectionListData()
+                .stream()
+                .sorted(Comparator.reverseOrder()).distinct()
+                .limit(5)
+                .collect(Collectors.toList());
+        listem.forEach((temp) -> {
+            System.out.println(temp);
+        });
+    }
+
+    //count: stream verilerinde veri sayısını hesaplar
+    public static void streamCountMethod(){
+       long counter=collectionListData()
+                .stream()
+                .sorted(Comparator.reverseOrder())
+                .count();
+        System.out.println(counter);
+    }
+
+    //filter: stream verilerindeki datalardan benim seçtiklerimi alsın
+    //filter: lambda expression kullanıyoruz
+    public static void streamFilterMethod(){
+        List<String> listem=collectionListData()
+                .stream()
+                .sorted(Comparator.reverseOrder())
+                .filter((temp)->"malatya".equalsIgnoreCase(temp))
+                .collect(Collectors.toList());
+        listem.forEach((temp) -> {
+            System.out.println(temp);
+        });
+    }
+
+
+    //map: stream verilerindeki her bir data üzerinden işlem yapabilmeye denir
+    //map: her bir eleman üzerinden işlem yapar
+    //map: lambda expression kullanıyoruz
+    public static void streamMapMethod(){
+        List<String> listem=collectionListData()
+                .stream()
+                .sorted(Comparator.reverseOrder())
+                .map((temp)->temp.toUpperCase())
+                .collect(Collectors.toList());
+        listem.forEach((temp) -> {
+            System.out.println(temp);
+        });
+    }
+
+
+    //reduce
+    //Match
+
     //PSVM
     public static void main(String[] args) {
         //ForEach
         //streamForEachMethod();
 
         //colect
-        streamCollectMethod();
+        //streamCollectMethod();
 
-    }
+        //sorted: Küçükten Büyüğe Sıralama
+        //streamSortedSmallToLargeMethod();
 
-}
+        //sorted: Küçükten Küçüğe Sıralama
+        //streamSortedLargeToSmallMethod();
+
+        //limit: Listede kısıtlama
+        //streamLimitMethod();
+
+        //Distinct: Tekrarsız veriler
+        //streamDistinctMethod();
+
+        //counter: verileri saymak
+        //streamCountMethod();
+
+        //filter: istediklerimi seçiyorum(seçmece karpuz)
+        //streamFilterMethod();
+
+        //map: Listedeki her bir veri üzerinden işlem yapabilmeye denir.
+        streamMapMethod();
+
+    } //end PSVM
+} //end Java8StreamTurorials
