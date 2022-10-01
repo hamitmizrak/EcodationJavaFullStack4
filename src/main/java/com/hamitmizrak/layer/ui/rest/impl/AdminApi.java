2be7d99a4ffa2b3149ff172165fd49730e9cc31a
hamitmizrak <hamitmizrak@gmail.com>
2022-10-01 15:47:08 +0300

    api3

--- src/main/java/com/hamitmizrak/layer/ui/rest/impl/AdminApi.java
+++ src/main/java/com/hamitmizrak/layer/ui/rest/impl/AdminApi.java
@@ -2,10 +2,7 @@ package com.hamitmizrak.layer.ui.rest.impl;
 
 import com.hamitmizrak.layer.business.dto.AdminDto;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin/v1")
@@ -23,6 +20,13 @@ public class AdminApi {
     @GetMapping("tutorialsapi2")
     public AdminDto getApi2(){
         AdminDto adminDto=AdminDto.builder().id(1L).name("adı").surname("soyadı").build();
+        return adminDto;
+    }
+
+    //http://localhost:8080/api/admin/v1/tutorialsapi3/44
+    @GetMapping("tutorialsapi3/{id}")
+    public AdminDto getApi3(@PathVariable(name = "id") Long adminId){
+        AdminDto adminDto=AdminDto.builder().id(adminId).name("adı").surname("soyadı").build();
         return adminDto;
     }
 }
